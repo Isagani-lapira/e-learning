35f6c4eb30e0b2cb1d77378ae53694637fe3db2b
Isagani Lapira <lapiraisagani@gmail.com>
2023-12-21 16:17:44 +0800

    Refractor: account app content

--- elearning/apps/accountapp/views.py
+++ elearning/apps/accountapp/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+def index(request):
+    template = "profile.html"
+    context = {
+        "name": "Isagani lapira Jr",
+    }
+    return render(request,template,context)
