044f29e0576162ad29619964b2886a47f222b0dc
Isagani Lapira <lapiraisagani@gmail.com>
2023-12-23 12:14:48 +0800

    Extends user authentication model and display on admin

--- elearning/apps/accountapp/admin.py
+++ elearning/apps/accountapp/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+#admin site diplay
+class CustomUser(UserAdmin):
+    model = User
+    list_display = ('username','email','first_name','last_name','is_instructor','is_staff')
+    fieldsets = [
+        (
+            None,
+            {
+                "fields":['username','email','first_name','last_name','is_instructor','is_staff']
+            }
+        )
+    ]
+    
+admin.site.register(User, CustomUser)
--- elearning/apps/accountapp/models.py
+++ elearning/apps/accountapp/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    #additional fields for user
+    is_instructor = models.BooleanField(default=False)
