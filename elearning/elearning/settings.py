35f29f712a82fa6590216aeacf07cdaf4af01dcc
Isagani Lapira <lapiraisagani@gmail.com>
2023-12-23 17:44:46 +0800

    Setup static and add jquery

--- elearning/elearning/settings.py
+++ elearning/elearning/settings.py
@@ -119,9 +119,9 @@ AUTH_USER_MODEL = "accountapp.User"  #built in User model
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    BASE_DIR, "static",
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 # Default primary key field type
