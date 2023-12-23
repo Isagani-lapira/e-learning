6f1256cbf421f9697f4d8770334aca6d0e8c1c31
mxykxlx <michaellashylle.ong.x@bulsu.edu.ph>
2023-12-24 02:49:03 +0800

    Add: Course App

--- elearning/elearning/urls.py
+++ elearning/elearning/urls.py
@@ -3,11 +3,13 @@ from django.shortcuts import render
 from django.urls import include, path
 from apps.accountapp import urls as account_url
 from apps.profileapp import urls as profile_url
+from apps.courseapp import urls as course_url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', lambda request: render(request, 'base.html'), name='empty_url'),
     path("__reload__/", include("django_browser_reload.urls")),
     path('account/', include(account_url)),
-    path('profile/', include(profile_url))
+    path('profile/', include(profile_url)),
+    path('course/', include(course_url)),
 ]
