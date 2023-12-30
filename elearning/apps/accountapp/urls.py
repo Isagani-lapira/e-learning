971c40a174934c1d733f1e7b18b5e24da28dfaf3
Isagani Lapira <lapiraisagani@gmail.com>
2023-12-30 12:46:02 +0800

    Refractor: login views and adding login session

--- elearning/apps/accountapp/urls.py
+++ elearning/apps/accountapp/urls.py
@@ -3,6 +3,6 @@ from apps.accountapp import views
 
 app_name = "accountapp"
 urlpatterns = [
-    path('',views.login,name="login_url"),
+    path('',views.login_user,name="login_url"),
     path('registration/',views.registration, name="register_url"),
 ]
