4cb084ff0a65c985097fb1488434a725fe5ef9ff
Isagani Lapira <lapiraisagani@gmail.com>
2024-01-03 14:54:18 +0800

    Refractor: Courseapp models

--- elearning/apps/courseapp/admin.py
+++ elearning/apps/courseapp/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
-from .models import Course
+from .models import *
 # Register your models here.
 
 class CourseAdmin(admin.ModelAdmin):
     list_display = ['instructor','title','date_created']
     
 admin.site.register(Course, CourseAdmin)
+
+
+class EnrollmentAdmin(admin.ModelAdmin):
+    list_display = ['student','course','date_enrolled','is_finish']
+    
+admin.site.register(Enrollment, EnrollmentAdmin)
