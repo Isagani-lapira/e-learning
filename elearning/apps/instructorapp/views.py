9d0423edda6bdea163a6c516c585822a848308f0
Isagani Lapira <lapiraisagani@gmail.com>
2024-01-03 15:07:33 +0800

    Display all the courses made by current instructor

--- elearning/apps/instructorapp/views.py
+++ elearning/apps/instructorapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import redirect, render, HttpResponse
 from django.contrib.auth.decorators import login_required
-from ..accountapp.models import User
+from ..accountapp.models import User, Instructor
+from ..courseapp.models import *
 # Create your views here.
 @login_required(login_url='/account/') #get back to login
 def dashboard(request):
@@ -9,10 +10,38 @@ def dashboard(request):
     
     if not is_instructor:
         #redirect to login and decide whether it should be on homepage or should log in first
-        print(is_instructor)
         return redirect('accountapp:login_url')
     else: 
         # go to dashboard page
+        instructor = Instructor.objects.get(user=request.user)
         templates = "dashboard.html"
-        context = {}
+        courses = GetCourses(instructor)
+        full_name = f"{request.user.first_name} {request.user.last_name}"
+        context = {
+            "courses":courses,
+            "instructor":full_name,
+        }
+
         return render(request,templates,context)
+    
+    
+def GetCourses(instructor):
+    course_obj = Course.objects.filter(instructor=instructor)
+
+    course_data = []
+
+    for course in course_obj:
+        total_modules = Module.objects.filter(course=course).count()
+        total_students = Enrollment.objects.filter(course=course).count()
+        print(total_modules)
+        course_info = {
+            "Title": course.title,
+            "Description": course.description,
+            "Image": course.course_img,
+            "Published_date": course.formatted_date(),
+            "Module_count":total_modules,
+            "Student_count":total_students,
+        }
+        course_data.append(course_info)
+
+    return course_data
