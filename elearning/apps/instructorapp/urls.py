7dd0c7e077709f54331c55c247e637dd4c6fd7ed
Isagani Lapira <lapiraisagani@gmail.com>
2024-01-07 12:35:10 +0800

    Refractor: instructor edit course template

--- elearning/apps/instructorapp/urls.py
+++ elearning/apps/instructorapp/urls.py
@@ -4,5 +4,6 @@ from apps.instructorapp import views
 app_name = "instructorapp"
 urlpatterns = [
     path('',views.dashboard, name="instructor_dashboard"),
-    path('addcourse/', views.AddCourse, name="instructo_addcourse")
+    path('addcourse/', views.AddCourse, name="instructo_addcourse"),
+    path('editcourse/<int:id>/',views.EditCourse, name="edit_url")
 ]
